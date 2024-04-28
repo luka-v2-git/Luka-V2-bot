290b7b07a9f70dfb92d2aa2fb0a40f46e8561814
Gost-bot-v2 <167943165+Gost-bot-v2@users.noreply.github.com>
2024-04-28 08:17:48 +0100

    notification.js

--- scripts/cmds/notification.js
+++ scripts/cmds/notification.js
@@ -31,7 +31,7 @@ module.exports = {
 		},
 		en: {
 			missingMessage: "Please enter the message you want to send to all groups",
-			notification: ".ↈ༈ 𝘿𝘼𝙍𝙆𝙉𝙀𝙎𝙎 ༈ↈ",
+			notification: ". 🌹𝐋𝐮𝐤𝐚 𝐑𝐊🌹 ",
 			sendingNotification: "Start sending notification from admin bot to %1 chat groups",
 			sentNotification: "✅ Sent notification to %1 groups successfully",
 			errorSendingNotification: "An error occurred while sending to %1 groups:\n%2"
@@ -43,7 +43,7 @@ module.exports = {
 		if (!args[0])
 			return message.reply(getLang("missingMessage"));
 		const formSend = {
-			body: `${getLang("notification")}\n\n═════════════════\n\n🌹⟨${args.join(" ")}⟩🌹\n\n═════════════════ ↈ༈ 𝘿𝘼𝙍𝙆𝙉𝙀𝙎𝙎 ༈ↈ`,
+			body: `${getLang("notification")}\n\n✰✰✰✰✰✰✰✰✰✰\n\n🌹⟨${args.join(" ")}⟩🌹\n\n✰✰✰✰✰✰✰✰✰✰🌹𝐋𝐮𝐤𝐚 𝐑𝐊 🌹 `,
 			attachment: await getStreamsFromAttachment(
 				[
 					...event.attachments,
