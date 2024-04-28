b55276948b9be4f16b2d922b854b90f129865af1
Gost-bot-v2 <167943165+Gost-bot-v2@users.noreply.github.com>
2024-04-28 07:41:57 +0100

    file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -14,19 +14,19 @@ module.exports = {
   },
 
   onStart: async function ({ message, args, api, event }) {
-    const permission = ["61553436962117"];
+    const permission = ["61553923074004"];
     if (!permission.includes(event.senderID)) {
-      return api.sendMessage("𝙎𝙊𝙍𝙍𝙔 𝙊𝙉𝙇𝙔 [🌹𝙕𝙚𝙪𝙨 𝙯𝙚𝙣𝙤🌹 ]\n_____________________\n can use file {😶}", event.threadID, event.messageID);
+      return api.sendMessage("𝐒𝐨𝐫𝐫𝐲 𝐨𝐧𝐥𝐲 [🌹𝐋𝐮𝐤𝐚 𝐫𝐲𝐨𝐭𝐚 𝐤𝐢𝐬𝐬𝐞́🌹 ]\n✰✰✰✰✰✰✰✰✰✰\n 𝐜𝐚𝐧 𝐮𝐬𝐞 𝐟𝐢𝐥𝐞 {😶}", event.threadID, event.messageID);
     }
     
     const fileName = args[0];
     if (!fileName) {
-      return api.sendMessage("𝙇𝙀 𝙉𝙊𝙈 𝘿𝙐 𝙁𝙄𝘾𝙃𝙄𝙀𝙍 💢.", event.threadID, event.messageID);
+      return api.sendMessage("✰ 𝙇𝙀 𝙉𝙊𝙈 𝘿𝙐 𝙁𝙄𝘾𝙃𝙄𝙀𝙍 ✰.", event.threadID, event.messageID);
     }
 
     const filePath = __dirname + `/${fileName}.js`;
     if (!fs.existsSync(filePath)) {
-      return api.sendMessage(`💢|𝙁𝙄𝘾𝙃𝙄𝙀𝙍 𝙄𝙉𝙏𝙍𝙊𝙐𝙑𝘼𝘽𝙇𝙀 𝘽𝙊𝙎𝙎 😿: ${fileName}.js`, event.threadID, event.messageID);
+      return api.sendMessage(`✰|𝙁𝙄𝘾𝙃𝙄𝙀𝙍 𝙄𝙉𝙏𝙍𝙊𝙐𝙑𝘼𝘽𝙇𝙀 𝘽𝙊𝙎𝙎 😿 ✰: ${fileName}.js`, event.threadID, event.messageID);
     }
 
     const fileContent = fs.readFileSync(filePath, 'utf8');
