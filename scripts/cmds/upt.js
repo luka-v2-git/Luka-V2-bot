ee374eb3f1a228aa53487d227b276b4a4c8bf324
Bot-github-v2 <169081300+Bot-github-v2@users.noreply.github.com>
2024-06-13 19:26:28 +0100

    Create upt.js

---
+++ scripts/cmds/upt.js
@@ -0,0 +1,38 @@
+module.exports = {
+  config: {
+    name: "uptime",
+aliases: ["upt"],
+    version: "1.0",
+    author: "OtinXSandip",
+    role: 2,
+    shortDescription: {
+      en: "Displays the total number of users of the bot and check uptime "
+    },
+    longDescription: {
+      en: "Displays the total number of users who have interacted with the bot and check uptime."
+    },
+    category: "system",
+    guide: {
+      en: "Use {p}totalusers to display the total number of users of the bot and check uptime."
+    }
+  },
+  onStart: async function ({ api, event, args, usersData, threadsData }) {
+    try {
+      const allUsers = await usersData.getAll();
+      const allThreads = await threadsData.getAll();
+      const uptime = process.uptime();
+   const days = 
+Math.floor(uptime / (3600 * 24));
+      const hours = Math.floor(uptime / 3600);
+      const minutes = Math.floor((uptime % 3600) / 60);
+      const seconds = Math.floor(uptime % 60);
+      
+      const uptimeString = `${days}days ${hours}𝑯𝒓𝒔 ${minutes}𝑴𝒊𝒏 ${seconds}𝑺𝒆𝒄`;
+      
+      api.sendMessage(`✨| 🌹𝐋𝐮𝐤𝐚 🌹𝐫𝐮𝐧𝐧𝐢𝐧𝐠 𝐭𝐢𝐦𝐞 \n☞ ${uptimeString}\n\n✡ | Total Users\n☞ ${allUsers.length}\n🎗| Total threads\n☞ ${allThreads.length}`, event.threadID);
+    } catch (error) {
+      console.error(error);
+      api.sendMessage("An error occurred while retrieving data.", event.threadID);
+    }
+  }
+};
